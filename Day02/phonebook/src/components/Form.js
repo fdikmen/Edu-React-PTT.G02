636ad09f25079533c3e77aa587ff04b6697a8eb1
FIRAT <frtdkmn@gmail.com>
2023-05-26 11:49:19 +0300

    Update Form.js

--- Day02/phonebook/src/components/Form.js
+++ Day02/phonebook/src/components/Form.js
@@ -18,24 +18,43 @@ export default class Form extends Component {
     console.log(this.state.name, this.state.phone)
   }*/
 
-  onChangeInput= (event) => {
+  onChangeInput = (event) => {
     this.setState({
       [event.target.name]: event.target.value
     })
     console.log(this.state.name, this.state.phone)
   }
 
+  onSubmitHandler = (e) => {
+    e.preventDefault() // prevent page refresh
+    /*this.props.addContact({
+      name: this.state.name,
+      phone: this.state.phone
+    })*/
+    // this.props.addContact(this.state)
+    if (this.state.name?.trim() !== '' && this.state.phone?.trim() !== '') {
+      this.props.addContact({ ...this.state })
+      this.setState({
+        name: '',
+        phone: ''
+      })
+    }
+    else {
+      alert('Please fill the form')
+    }
+  }
+
 
 
   render() {
     return (
-      <div className='FormComponent'>
-        <input type="text" name='name' id='name' 
-        placeholder="Name" onChange={this.onChangeInput} />
-        <input type="text" name='phone' id='phone' 
-        placeholder="Phone" onChange={this.onChangeInput} />
-        <button type="button">Add</button>
-      </div>
+      <form className='FormComponent' onSubmit={this.onSubmitHandler}>
+        <input type="text" name='name' id='name' value={this.state.name}
+          placeholder="Name" onChange={this.onChangeInput} />
+        <input type="text" name='phone' id='phone' value={this.state.phone}
+          placeholder="Phone" onChange={this.onChangeInput} />
+        <button type="submit">Add</button>
+      </form>
     )
   }
 }
